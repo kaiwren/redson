8d1e12d665dc710f59fdace89189584305977977
Sidu Ponnappa <ckponnappa@gmail.com>
2014-11-04 01:44:22 +0530

    Rename Opal001 to Redson, missed a piece of text

--- app/hello.rb
+++ app/hello.rb
@@ -4,6 +4,6 @@ end
 require "hello/greeter"
 
 Document.ready? do
-  puts "Hello! Opal 001 Version: #{Redson::VERSION}"
+  puts "Hello! Using Redson Version: #{Redson::VERSION}"
   Hello::Greeter.new(Element["#greeter"]).render
 end
