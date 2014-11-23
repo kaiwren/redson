c04ea156180dd6751d2eaeec61a416e103cf6637
Sidu Ponnappa <ckponnappa@gmail.com>
2014-11-23 15:50:55 +0530

    Added a link to the benchmarks page

--- rails-app/config/routes.rb
+++ rails-app/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :students
-  get 'benchmarks' => 'benchmarks#index'
+  get 'benchmarks' => 'benchmarks#index', :as => :benchmarks
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
